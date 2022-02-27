de3d395fb6e64763b9db540db2aa144d823ddfb5
P22srogers <p22srogers@fairfieldprep.org>
2022-02-27 09:02:03 -0500

    DONE

---
+++ lab_12-5.py
@@ -0,0 +1,6 @@
+# Author: SMR (AMDG) 2/27/22
+# Set variable row equal to assignment of names per number computer. Wasn't sure which numbers they were, so I made ones myself.
+row = {"B202-15":"James", "B202-14":"Ryan", "B202-13": "Luis", "B202-12": "Sean"}
+
+# Print out the variable row
+print(row)
